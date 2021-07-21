da9bf43ae069da8a1c6beb2bc6ef3356ad5666a8
DANGTHIBICHTRUYEN <80092695+DANGTHIBICHTRUYEN@users.noreply.github.com>
2021-07-21 10:17:04 -0500

    nhập xuất

---
+++ NHẬP XUẤT DỮ LIỆU 2/tank.js
@@ -0,0 +1,24 @@
+function tren(){
+    let top = parseInt(document.getElementById("tank").style["top"]);
+    document.getElementById("tank").style["top"] = top - 19 + "px";
+
+}
+function duoi(){
+    let top = parseInt(document.getElementById("tank").style["top"]);
+    document.getElementById("tank").style["top"] = top + 19 + "px";
+
+}
+
+function trai(){
+    let left = parseInt(document.getElementById("tank").style["left"]);
+    document.getElementById("tank").style["left"] = left - 19 + "px";
+
+}
+
+function phai(){
+    let left = parseInt(document.getElementById("tank").style["left"]);
+    document.getElementById("tank").style["left"] = left + 19 + "px";
+
+}
+
+
